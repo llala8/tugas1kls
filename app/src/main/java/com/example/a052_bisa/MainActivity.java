cad3fb7850ade8ccb55ca0b93b3331e7530806f3
llala8 <lalalailatul34@gmail.com>
2021-10-17 03:31:11 -0700

    first commit

---
+++ app/src/main/java/com/example/a052_bisa/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.a052_bisa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+
+        });
+    }
+}
